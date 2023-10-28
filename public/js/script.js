c478dd7b6c8a9543a93e598c44a2207a31066bba
apnagaadi <apnaagaadi@gmail.com>
2023-10-28 22:47:45 +0530

    scroll

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
 
 
 
-  /*scroll */
+  /*scroll 
   
     const testimonialCarousel = document.querySelector('.testimonial-carousel');
 
@@ -22,5 +22,72 @@ document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
         });
     }
 
-    setInterval(autoScroll, 5000); // Adjust the interval as needed
+    setInterval(autoScroll, 5000); */
 
+    const testimonialCarousel = document.querySelector('.testimonial-carousel');
+    const dots = document.querySelectorAll('.dot');
+    const prevArrow = document.querySelector('.prev-arrow');
+    const nextArrow = document.querySelector('.next-arrow');
+    let currentTestimonial = 1;
+    let autoScrollInterval;
+    function changeTestimonial(testimonialNumber) {
+        if (testimonialNumber === currentTestimonial) {
+            return;
+        }
+    
+        // Remove the active class from the current dot
+        dots[currentTestimonial - 1].classList.remove('active-dot');
+    
+        // Scroll to the selected testimonial
+        testimonialCarousel.scrollTo({
+            left: (testimonialNumber - 1) * testimonialCarousel.clientWidth,
+            behavior: 'smooth',
+        });
+    
+        // Set the new current testimonial and add active class to the corresponding dot
+        currentTestimonial = testimonialNumber;
+        dots[currentTestimonial - 1].classList.add('active-dot');
+    }
+    
+    function prevTestimonial() {
+        newTestimonial = (currentTestimonial + 1 > 4) ? 1 : currentTestimonial + 1;
+        changeTestimonial(newTestimonial);
+    }
+    
+    function nextTestimonial() {
+        newTestimonial = (currentTestimonial - 1 < 1) ? 4 : currentTestimonial - 1;
+        changeTestimonial(newTestimonial);
+    }
+
+    function startAutoScroll() {
+        autoScrollInterval = setInterval(nextTestimonial, 5000);
+    }
+    
+    function stopAutoScroll() {
+        clearInterval(autoScrollInterval);
+    }
+    
+    // Initialize by setting the first dot as active
+    dots[currentTestimonial - 1].classList.add('active-dot');
+    startAutoScroll();
+    // Add event listeners to the previous and next arrows
+prevArrow.addEventListener('click', prevTestimonial);
+nextArrow.addEventListener('click', nextTestimonial);
+    // Change testimonial every 5 seconds
+    prevArrow.addEventListener('click', () => {
+        prevTestimonial();
+        stopAutoScroll();
+    });
+    nextArrow.addEventListener('click', () => {
+        nextTestimonial();
+        stopAutoScroll();
+    });
+    
+    // Add event listeners to the dots
+    dots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            changeTestimonial(index + 1);
+            stopAutoScroll();
+        });
+    }); 
+    
